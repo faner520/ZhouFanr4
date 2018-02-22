a30f49b8692c8922b34d02387ed29f9746a20f2f
guang_liang_ <845774766@qq.com>
2018-02-22 20:46:54 +0800

    第一次更新

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function(){
+
+
+	/* ---- Countdown timer ---- */
+
+	$('#counter').countdown({
+		timestamp : (new Date("2018-03-05 00:00:00")).getTime()
+	});
+
+
+	/* ---- Animations ---- */
+
+	$('#links a').hover(
+		function(){ $(this).animate({ left: 3 }, 'fast'); },
+		function(){ $(this).animate({ left: 0 }, 'fast'); }
+	);
+
+	$('footer a').hover(
+		function(){ $(this).animate({ top: 3 }, 'fast'); },
+		function(){ $(this).animate({ top: 0 }, 'fast'); }
+	);
+
+
+
+
+});
